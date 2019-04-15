9a649fcc8372b19555a6090aafafd9520d69f075
Warut Surapat <s.warut@gmail.com>
2019-04-16 00:23:43 +0900

    Wip on fixing tests.

--- test/lib/crawler/http_test.exs
+++ test/lib/crawler/http_test.exs
@@ -1,46 +1,51 @@
 defmodule Crawler.HTTPTest do
   use Crawler.TestCase, async: true
+  require IEx
 
   alias Crawler.HTTP
 
   doctest HTTP
 
   test "default user agent", %{bypass: bypass, url: url} do
+
     Agent.start_link(fn -> "" end, name: HTTP.DefaultUA)
-
     Bypass.expect_once bypass, "GET", "/http/default_ua", fn (conn) ->
-      {_, ua} = Enum.find(conn.req_headers, fn({"user-agent", _}) -> true end)
 
+      IO.puts("conn.req_headers : #{inspect conn}")
+      IO.puts "----------"
+
+      {_, ua} = Enum.find(conn.req_headers, fn({k, _}) -> k == "user-agent" end)
       Agent.update(HTTP.DefaultUA, fn(_) -> ua end)
 
       Plug.Conn.resp(conn, 200, "")
     end
+    HTTPoison.start
+    HTTPoison.get! "#{url}/http/default_ua"
+    # Crawler.crawl("#{url}/http/default_ua")
 
-    Crawler.crawl("#{url}/http/default_ua")
-
-    wait fn ->
-      assert String.match?(
-        Agent.get(HTTP.DefaultUA, & &1),
-        ~r{Crawler/\d\.\d\.\d \(https://github\.com/fredwu/crawler\)}
-      )
-    end
+    # wait fn ->
+    #   assert String.match?(
+    #     Agent.get(HTTP.DefaultUA, & &1),
+    #     ~r{Crawler/\d\.\d\.\d \(https://github\.com/fredwu/crawler\)}
+    #   )
+    # end
   end
 
-  test "custom user agent", %{bypass: bypass, url: url} do
-    Agent.start_link(fn -> "" end, name: HTTP.CustomUA)
+  # test "custom user agent", %{bypass: bypass, url: url} do
+  #   Agent.start_link(fn -> "" end, name: HTTP.CustomUA)
 
-    Bypass.expect_once bypass, "GET", "/http/custom_ua", fn (conn) ->
-      {_, ua} = Enum.find(conn.req_headers, fn({"user-agent", _}) -> true end)
+  #   Bypass.expect_once bypass, "GET", "/http/custom_ua", fn (conn) ->
+  #     {_, ua} = Enum.find(conn.req_headers, fn({"user-agent", _}) -> true end)
 
-      Agent.update(HTTP.CustomUA, fn(_) -> ua end)
+  #     Agent.update(HTTP.CustomUA, fn(_) -> ua end)
 
-      Plug.Conn.resp(conn, 200, "")
-    end
+  #     Plug.Conn.resp(conn, 200, "")
+  #   end
 
-    Crawler.crawl("#{url}/http/custom_ua", user_agent: "Hello World")
+  #   Crawler.crawl("#{url}/http/custom_ua", user_agent: "Hello World")
 
-    wait fn ->
-      assert Agent.get(HTTP.CustomUA, & &1) == "Hello World"
-    end
-  end
+  #   wait fn ->
+  #     assert Agent.get(HTTP.CustomUA, & &1) == "Hello World"
+  #   end
+  # end
 end
